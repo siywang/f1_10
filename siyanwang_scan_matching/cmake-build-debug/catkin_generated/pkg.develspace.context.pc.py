1211a6dfb18990f7369f1a1dfa3e88d09b6942b9
siywang <siyan541@gmail.com>
2019-10-05 15:33:27 -0400

    scan matching lab code 1

---
+++ siyanwang_scan_matching/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "nav_msgs;roscpp;rospy;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "siyanwang_scan_matching"
+PROJECT_SPACE_DIR = "/home/siyan/SiyanWang_ws/src/f1_10/siyanwang_scan_matching/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ siyanwang_scan_matching/cmake-build-debug/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/siyan/SiyanWang_ws/src/f1_10/siyanwang_scan_matching/cmake-build-debug/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
